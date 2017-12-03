738469f0d6b2fa49dc139d152da051162f5890e8
Truong Sinh <ntruongsinh96@gmail.com>
2017-12-03 20:59:43 +0700

    change testcase

--- src/test/java/testcase/CalculatorTest.java
+++ src/test/java/testcase/CalculatorTest.java
@@ -17,7 +17,7 @@ public class CalculatorTest {
         assertEquals(0, c.multiply(0, 0), "0 x 0 must be 0");
 	}
 	@Test
-	public void  multiplication(){
+	public void  multiplicationofOneShouldReturnItself(){
 		Calculator c = new Calculator();
 		assertEquals(20, c.multiply(10, 1), "10 x 1 must be 20");
 	}
