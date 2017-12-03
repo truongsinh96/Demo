f724d7d4be07bd74f92e855dcda1233047659d7f
Truong Sinh <ntruongsinh96@gmail.com>
2017-12-03 13:45:41 +0700

    dd

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -1,13 +1,39 @@
 package controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class Main {
-	@RequestMapping("/")
-	public String getIndex(){
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String getIndex() {
+
 		return "index";
+	}
+
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public String postIndex(@RequestParam Date date, Model m) {
+		Integer.parseInt("aaa");
+		m.addAttribute(date);
+		return "index";
+	}
+
+	@ExceptionHandler
+	public ModelAndView handleException(Exception e){
+		ModelAndView m = new ModelAndView();
+		m.setViewName("error");
+		m.addObject("message", e.getMessage());
+		return m;
 	}
 }
--- src/main/webapp/WEB-INF/jsp/error.jsp
+++ src/main/webapp/WEB-INF/jsp/error.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
-    <%@ page isELIgnored = "false" %>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -8,10 +8,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-<form method="post">
-<input name = "date" type="date"/>
-<input type="submit">
-</form>
-${date}
+${message }
 </body>
 </html>
